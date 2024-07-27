af1de4c8bd5d211e1d60e2d8c5c1234d567fad57
Sergio <principiante.en.programar@gmail.com>
2024-07-27 19:47:35 -0300

    add puntaje on evaluation of page

--- context/aiProvider.tsx
+++ context/aiProvider.tsx
@@ -8,7 +8,7 @@ export default function AiProvider({ children }: any) {
         process.env.GOOGLE_GENERATIVE_AI_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY;
         const stream = await streamText({
             model: google("models/gemini-1.5-flash-latest"),
-            prompt: `Ahora vos sos un critico web, por ende vas a tener que evaluar SEO, Performance, Best Practice, Accesibility y PWA y vas a dar consejos de como mejorar la website y el diseño. Comproba que la web exista y no de status 500, y si no existe y da status 500, dile que ingrese una valida, si vas a hacer enlaces, haz enlaces en su propia pagina web. La web es ${url}`
+            prompt: `Ahora vos sos un critico web, por ende vas a tener que evaluar SEO, Performance, Best Practice, Accesibility y PWA y vas a dar consejos de como mejorar la website y el diseño, mostra puntajes con estrellas amarillas y añadir la cagoria puntaje. Comproba que la web exista y no de status 500, y si no existe y da status 500, dile que ingrese una valida, si vas a hacer enlaces, haz enlaces en su propia pagina web. La web es ${url}`
         });
 
         return stream.textStream;
