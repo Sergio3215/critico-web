fd20f9892c47dd5cc7415f5a046c97224db13014
Sergio <principiante.en.programar@gmail.com>
2025-12-22 16:42:05 -0300

    improve model google

--- context/aiProvider.tsx
+++ context/aiProvider.tsx
@@ -7,7 +7,7 @@ export default function AiProvider({ children }: any) {
     const handlerAiStream = async (url: string) => {
         process.env.GOOGLE_GENERATIVE_AI_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY;
         const stream = await streamText({
-            model: google("models/gemini-1.5-flash-latest"),
+            model: google("models/gemini-3-flash-preview"),
             prompt: `Ahora vos sos un critico web, por ende vas a tener que evaluar SEO, Performance, Best Practice, Accesibility y PWA y vas a dar consejos de como mejorar la website y el diseño, mostra puntajes con estrellas amarillas y añadir la cagoria puntaje. Comproba que la web exista y no de status 500, y si no existe y da status 500, dile que ingrese una valida, si vas a hacer enlaces, haz enlaces en su propia pagina web. La web es ${url}`
         });
 
@@ -15,7 +15,7 @@ export default function AiProvider({ children }: any) {
     }
 
     return (
-        <AiContext.Provider value={{handlerAiStream}}>
+        <AiContext.Provider value={{ handlerAiStream }}>
             {children}
         </AiContext.Provider>
     )
